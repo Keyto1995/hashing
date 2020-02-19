c126a6e47c3b83ee92bda6750155b30498c8e928
keyto1995 <keyto1995@outlook.com>
2020-02-19 21:30:07 +0800

    go语言缩进符使用制表符
    Signed-off-by: keyto1995 <keyto1995@outlook.com>

--- hashing_test.go
+++ hashing_test.go
@@ -1,30 +1,30 @@
 package hashing
 
 import (
-    "bytes"
-    "testing"
+	"bytes"
+	"testing"
 )
 
 func TestMurmur32(t *testing.T) {
-    buffer := bytes.NewBufferString("hello, world")
-    hashFunction := Murmur32()
-    hashCode := hashFunction.HashBytes(buffer.Bytes())
+	buffer := bytes.NewBufferString("hello, world")
+	hashFunction := Murmur32()
+	hashCode := hashFunction.HashBytes(buffer.Bytes())
 
-    t.Logf("编码长度：%v bits", hashCode.Bits())
-    t.Logf("uint32: %v", hashCode.AsUint32())
-    t.Logf("int32: %v", int32(hashCode.AsUint32()))
-    t.Logf("bytes: %v", hashCode.AsBytes())
-    t.Logf("hex: %v", hashCode.AsHex())
+	t.Logf("编码长度：%v bits", hashCode.Bits())
+	t.Logf("uint32: %v", hashCode.AsUint32())
+	t.Logf("int32: %v", int32(hashCode.AsUint32()))
+	t.Logf("bytes: %v", hashCode.AsBytes())
+	t.Logf("hex: %v", hashCode.AsHex())
 }
 
 func TestMurmur32_1(t *testing.T) {
-    hasher := Murmur32().NewHasher()
-    hasher.PutString("hello").PutByte(byte(',')).PutString(" world")
-    hashCode := hasher.Hash()
+	hasher := Murmur32().NewHasher()
+	hasher.PutString("hello").PutByte(byte(',')).PutString(" world")
+	hashCode := hasher.Hash()
 
-    t.Logf("编码长度：%v bits", hashCode.Bits())
-    t.Logf("uint32: %v", hashCode.AsUint32())
-    t.Logf("int32: %v", int32(hashCode.AsUint32()))
-    t.Logf("bytes: %v", hashCode.AsBytes())
-    t.Logf("hex: %v", hashCode.AsHex())
+	t.Logf("编码长度：%v bits", hashCode.Bits())
+	t.Logf("uint32: %v", hashCode.AsUint32())
+	t.Logf("int32: %v", int32(hashCode.AsUint32()))
+	t.Logf("bytes: %v", hashCode.AsBytes())
+	t.Logf("hex: %v", hashCode.AsHex())
 }
