054b02c64c9a4a74f2d1777497b204710bbbe66a
keyto1995 <keyto1995@outlook.com>
2020-02-19 21:10:59 +0800

    统一tab为空格
    Signed-off-by: keyto1995 <keyto1995@outlook.com>

--- murmur3/mm32/hash_function_test.go
+++ murmur3/mm32/hash_function_test.go
@@ -1,51 +1,51 @@
 package mm32
 
 import (
-    "github.com/keyto1995/hashing/hashcode"
+	"github.com/keyto1995/hashing/hashcode"
 	"reflect"
-    "testing"
+	"testing"
 )
 
 func TestMurmur3_32HashFunction_HashBytes(t *testing.T) {
-    type fields struct {
-        seed uint32
-    }
-    type args struct {
-        input []byte
-    }
-    tests := []struct {
-        name   string
-        fields fields
-        args args
-        want hashcode.HashCode
-    }{
-        {
-            name:   "",
-            fields: fields{0},
-            args:   args{[]byte{}},
-            want:   hashcode.NewUint32HashCode(0),
-        },
-        {
-            name:   "",
-            fields: fields{0},
-            args:   args{[]byte{1, 2, 3, 4, 5, 6}},
-            want:   hashcode.NewUint32HashCode(3619664446),
-        },
-        {
-            name:   "",
-            fields: fields{0},
-            args:   args{[]byte{1, 2, 3, 4, 5, 7}},
-            want:   hashcode.NewUint32HashCode(337367262),
-        },
-    }
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            m := &HashFunction{
-                seed: tt.fields.seed,
-            }
-            if got := m.HashBytes(tt.args.input); !reflect.DeepEqual(got, tt.want) {
-                t.Errorf("HashBytes() = %v, want %v", got, tt.want)
-            }
-        })
-    }
+	type fields struct {
+		seed uint32
+	}
+	type args struct {
+		input []byte
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   hashcode.HashCode
+	}{
+		{
+			name:   "",
+			fields: fields{0},
+			args:   args{[]byte{}},
+			want:   hashcode.NewUint32HashCode(0),
+		},
+		{
+			name:   "",
+			fields: fields{0},
+			args:   args{[]byte{1, 2, 3, 4, 5, 6}},
+			want:   hashcode.NewUint32HashCode(3619664446),
+		},
+		{
+			name:   "",
+			fields: fields{0},
+			args:   args{[]byte{1, 2, 3, 4, 5, 7}},
+			want:   hashcode.NewUint32HashCode(337367262),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &HashFunction{
+				seed: tt.fields.seed,
+			}
+			if got := m.HashBytes(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("HashBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
